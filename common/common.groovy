68d5f71949efbfe92344a87d85ad297bab159279
ranjithanagaraj <43988647+ranjithanagaraj@users.noreply.github.com>
2019-07-14 21:27:25 +0530

    Update common.groovy

--- common/common.groovy
+++ common/common.groovy
@@ -11,35 +11,34 @@ def uploadWarArtifactory() {
 			}]
 		}"""
 		server.upload(uploadSpec) 	
+		}
 	}
-}
 
 def sonar(){
-	withSonarQubeEnv('sonarqube') {
-	def mvncmd=props.SONAR_SCAN
-	sh "${mvncmd}"
-	}
+	  withSonarQubeEnv('sonarqube') {
+	  def mvncmd=props.SONAR_SCAN
+	  sh "${mvncmd}"
+	   }
 }
-
-    def	qualityGate() {
-        timeout(time: 10, unit: 'MINUTES') {
+def qualityGate() {
+           timeout(time: 10, unit: 'MINUTES') {
            def qg = waitForQualityGate() 
            if (qg.status != 'OK') {
              error "Pipeline aborted due to quality gate failure: ${qg.status}"
            }
-      }
-    	}
+           }
+    	   }
 
 def sendEmail() {
-		 emailext body: '${DEFAULT_CONTENT}', subject: '${DEFAULT_SUBJECT}', to:  props.RECEPIENT_MAIL_ID
+	  emailext body: '${DEFAULT_CONTENT}', subject: '${DEFAULT_SUBJECT}', to:  props.RECEPIENT_MAIL_ID
 
 }
 
 def failureEmail(err) {
 	emailext( 
-			subject: '${JOB_NAME} - BUILD # ${BUILD_NUMBER} -  FAILURE', 
-		         body: "${err}",
-		        to: props.RECEPIENT_MAIL_ID
+		subject: '${JOB_NAME} - BUILD # ${BUILD_NUMBER} -  FAILURE', 
+		body: "${err}",
+		to: props.RECEPIENT_MAIL_ID
 		);
 	print 'mail sent'
 }
